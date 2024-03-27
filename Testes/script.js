307cfb5670fc3f6a1531885346c8f496ecefc0f0
JoaoVBonifácio <jboni2014@gmail.com>
2024-03-27 18:23:41 -0300

    Portifólio 2.0
    0.0.2

---
+++ Testes/script.js
@@ -0,0 +1,7 @@
+function trocarDiv() {
+    var div1 = document.getElementById('div1');
+    var div2 = document.getElementById('div2');
+
+    div1.classList.toggle('hidden');
+    div2.classList.toggle('hidden');
+}
