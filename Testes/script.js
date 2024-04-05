eff6985db2f728e26c66425a8116bee656a28218
JoaoVBonifácio <jboni2014@gmail.com>
2024-04-05 19:49:47 -0300

    Portifólio 2.0
    Version Desktop feita
    
    Falta a mobile.

--- Testes/script.js
+++
@@ -1,7 +0,0 @@
-function trocarDiv() {
-    var div1 = document.getElementById('div1');
-    var div2 = document.getElementById('div2');
-
-    div1.classList.toggle('hidden');
-    div2.classList.toggle('hidden');
-}
